a78d1da99f681d6f56f49f2a302c1457e8b553f7
chanbingim <127704542+chanbingim@users.noreply.github.com>
2026-07-06 11:48:29 +0900

    이벤트 버스 추가

--- Assets/Scripts/Manager/Game/AddressablesManager.cs
+++ Assets/Scripts/Manager/Game/AddressablesManager.cs
@@ -1,11 +1,8 @@
-using AutoGroupGenerator;
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
-using UnityEngine.InputSystem;
 using UnityEngine.ResourceManagement.AsyncOperations;
 using UnityEngine.U2D;
 
