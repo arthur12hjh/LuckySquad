6924bedadae89f74c8a485b69051a02c55c19088
chanbingim <127704542+chanbingim@users.noreply.github.com>
2026-07-09 16:44:34 +0900

    도트뎀 추가

--- Assets/Scripts/Entity/Item/Equipment/Weapon/Projectile/HolyWater.cs
+++ Assets/Scripts/Entity/Item/Equipment/Weapon/Projectile/HolyWater.cs
@@ -2,22 +2,18 @@ using Attack;
 using DG.Tweening;
 using Item;
 using UnityEngine;
+using static EfffectRef;
 
 public class HolyWater : ProjectileBase
 {
     [SerializeField] LayerMask LayerMask;
 
-    Animator   animator = null;
+
     Vector3     TargetPoint = Vector3.zero;
-    
-    void Awake()
-    {
-        animator = GetComponent<Animator>();
-    }
 
     private void OnEnable()
-    {
-        if(animator == null)
+    { 
+        if(animator  == null)
             animator = GetComponent<Animator>();
 
         animator.speed = 0f;
@@ -35,14 +31,40 @@ public class HolyWater : ProjectileBase
         {
             hit.GetComponent<Monster>()?.Damaged(gameObject, new SAttackData(20));
         }
+
+        foreach (var particleID in _levelData.ParticleIDs)
+        {
+            var refSO = DataManager.Instance.FindEffectSO(particleID);
+            var obj = ObjectPoolManager.Instance.Get(refSO);
+
+            var efffectRef = refSO as EfffectRef;
+            obj.gameObject.transform.position = transform.position;
+            obj.SetActive(true);
+            if (efffectRef._Type == EEffectSOType.DamageEffect)
+            {
+                if (_levelData.ItemEffects.TryGetValue(EEffectType.Damage, out var list))
+                {
+                    var FogEft = obj.gameObject.GetComponent<GroundFogEffect>();
+                    if(FogEft != null)
+                    {
+                        FogEft.Initailize(info, list[0] as DamageEffect);
+                    }
+                }
+            }
+         }
     }
 
-    public override void ShootProjectile(Projectileinfo projectileinfo, Vector2 vdir, string AtalsName)
+    protected override void ShootProjectile(Projectileinfo projectileinfo,
+                                         Vector2 vdir,
+                                         string AtalsName,
+                                         string ControolerName)
     {
-        base.ShootProjectile(projectileinfo, vdir, AtalsName);
+        base.ShootProjectile(projectileinfo, vdir, AtalsName, ControolerName);
 
         if (SpriteTexs.Length > projectileinfo.iLevel)
             spriteRenderer.sprite = SpriteTexs[0];
+
+        TargetPoint = UtilitySystem.GetRandomWorldPoint(Camera.main);
 
         transform.DOMove(TargetPoint, 1)
             .OnComplete(() =>
