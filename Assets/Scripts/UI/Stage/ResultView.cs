8f7f69e9b345694b6de82b9cc33e35f8f60925b2
JinhoBa <127280695+JinhoBa@users.noreply.github.com>
2026-07-09 19:28:15 +0900

    - UI 테마 변경 - Card Grade 로직 추가 - 폰트 추가,

---
+++ Assets/Scripts/UI/Stage/ResultView.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResultView : BaseView
+{
+    [SerializeField] Button ExitButton;
+    [SerializeField] GameObject Panel;
+
+    private void Start()
+    {
+        ExitButton.onClick.AddListener(InGameManager.Instance.EndStage);
+        InGameManager.Instance.OnGameClear += Show;
+    }
+
+    public override void Show()
+    {
+        Panel.SetActive(true);
+    }
+
+    private void OnDestroy()
+    {
+        if(InGameManager.Instance != null)
+            InGameManager.Instance.OnGameClear -= Show;
+    }
+}
