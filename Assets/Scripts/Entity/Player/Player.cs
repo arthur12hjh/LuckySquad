42866b366160a68f547cebee3a7afd20c8b4e25b
한재훈 <arthur12hjh@naver.com>
2026-07-21 14:15:00 +0900

    플레이어 피격

--- Assets/Scripts/Entity/Player/Player.cs
+++ Assets/Scripts/Entity/Player/Player.cs
@@ -7,7 +7,10 @@ public class Player : BaseEntity
     
     private static readonly int _isMoveID = Animator.StringToHash("isMove");
     private static readonly int _directionID = Animator.StringToHash("Direction");
-    
+
+    // Monster Layer 캐싱. Layer 비교로 충돌 대상을 판단한다
+    private int _monsterLayer;
+
     public override void Initialize(ScriptableObject initRef)
     {
     }
@@ -20,16 +23,59 @@ public class Player : BaseEntity
     void Awake()
     {
         _animator = GetComponent<Animator>();
+        _monsterLayer = LayerMask.NameToLayer("Monster");
     }
 
     public void UpdatePlayer(Vector2 inputVec)
     {
         _animator.SetBool(_isMoveID, !Mathf.Approximately(inputVec.magnitude, 0f));
-        
+
         if (!Mathf.Approximately(inputVec.x, 0f))
         {
             _animator.SetFloat(_directionID, inputVec.x);
         }
     }
-    
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // 충돌 대상이 Monster Layer인 경우에만 데미지 처리
+        if (other.gameObject.layer != _monsterLayer)
+        {
+            return;
+        }
+
+        // MonsterRef의 Power만큼 플레이어가 데미지를 받는다
+        Monster monster = other.GetComponent<Monster>();
+        if (monster is null)
+        {
+            return;
+        }
+
+        TakeDamage(monster.Power);
+    }
+
+    private void TakeDamage(float damage)
+    {
+        // 실제 플레이어 HP는 PlayerStats가 관리하므로 InGameManager를 통해 접근한다
+        if (InGameManager.Instance is null)
+        {
+            return;
+        }
+
+        PlayerStats stats = InGameManager.Instance.GetPlayerStats();
+        if (stats is null)
+        {
+            return;
+        }
+
+        float nextHp = stats.CurrentHp - damage;
+        Debug.Log($"Damaged : {nextHp}");
+        if (nextHp < 0f)
+        {
+            nextHp = 0f;
+        }
+
+        stats.CurrentHp = nextHp;
+    }
+
 }
--- Assets/Scripts/Entity/Player/PlayerStats.cs
+++ Assets/Scripts/Entity/Player/PlayerStats.cs
@@ -23,7 +23,7 @@ public class PlayerStats
     [SerializeField] private float _power;
     [SerializeField] private float _speed;
 
-    private int BASE_EXPERIENCE = 200;
+    private int BASE_EXPERIENCE = 50;
     private const int maxLevel = 50;
     
     public PlayerStats(PlayerStatsRef refSO)
